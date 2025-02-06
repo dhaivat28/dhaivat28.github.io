8a2ec04e748f7998ad7c206a2065aeb24c4b8668
Dhaivat Parikh <dhaivat28@gmail.com>
2025-02-07 00:45:50 +1100

    Added new react project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="border-4 border-red-600">
+      <h1 className="text-5xl font-bold underline border-4 border-red-600">
+        Hello worl232d!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
